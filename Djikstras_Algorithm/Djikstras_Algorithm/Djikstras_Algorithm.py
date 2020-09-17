87d7f79771da3cf042c17dae7a6294f036340d9c
Jason Riha <superjar30@gmail.com>
2020-09-17 13:28:59 -0500

    cleaned up code

--- Djikstras_Algorithm/Djikstras_Algorithm/Djikstras_Algorithm.py
+++ Djikstras_Algorithm/Djikstras_Algorithm/Djikstras_Algorithm.py
@@ -2,23 +2,14 @@ from collections import defaultdict
 
 class Graph():
     def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        self.weights has all the weights between two nodes,
-        with the two nodes as a tuple as the key
-        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-        """
         self.edges = defaultdict(list)
         self.weights = {}
     
     def add_edge(self, from_node, to_node, weight):
-        # Note: assumes edges are bi-directional
         self.edges[from_node].append(to_node)
         self.edges[to_node].append(from_node)
         self.weights[(from_node, to_node)] = weight
         self.weights[(to_node, from_node)] = weight
-
 
 graph = Graph()
 
@@ -46,12 +37,10 @@ for edge in edges:
     graph.add_edge(*edge)
 
 def dijsktra(graph, initial, end):
-    # shortest paths is a dict of nodes
-    # whose value is a tuple of (previous node, weight)
     shortest_paths = {initial: (None, 0)}
     current_node = initial
     visited = set()
-    
+
     while current_node != end:
         visited.add(current_node)
         destinations = graph.edges[current_node]
@@ -71,16 +60,13 @@ def dijsktra(graph, initial, end):
         next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
         if not next_destinations:
             return "Route Not Possible"
-        # next node is the destination with the lowest weight
         current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
     
-    # Work back through destinations in shortest path
     path = []
     while current_node is not None:
         path.append(current_node)
         next_node = shortest_paths[current_node][0]
         current_node = next_node
-    # Reverse path
     path = path[::-1]
     print("Here is the shortest path from " + initial + " to " + end + ": ", path)
     return path
